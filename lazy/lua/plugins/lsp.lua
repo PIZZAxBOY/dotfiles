194b302e238f2faddafb75ff13f1f4e83b62ba91
belugua <belugua0@qq.com>
2025-07-08 20:46:42 +0800

    new config

--- lazy/lua/plugins/lsp.lua
+++ lazy/lua/plugins/lsp.lua
@@ -1,74 +1,155 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-"lua-language-server",
-	},
-    },
-    opts_extend = { "ensure_installed" },
-    config = function(_, opts)
-      require("mason").setup(opts)
-      local mr = require("mason-registry")
-
-      local function ensure_installed()
-        for _, tool in ipairs(opts.ensure_installed) do
-          local p = mr.get_package(tool)
-          if not p:is_installed() then
-            p:install()
-          end
-        end
-      end
-      if mr.refresh then
-        mr.refresh(ensure_installed)
-      else
-        ensure_installed()
-      end
-    end,
-  },
 	{
-  'neovim/nvim-lspconfig',
-  dependencies = { 'saghen/blink.cmp' },
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"typescript-language-server",
+				"marksman",
+			},
+		},
+		opts_extend = { "ensure_installed" },
+		config = function(_, opts)
+			require("mason").setup(opts)
+			local mr = require("mason-registry")
 
- -- example calling setup directly for each LSP
-  config = function()
+			local function ensure_installed()
+				for _, tool in ipairs(opts.ensure_installed) do
+					local p = mr.get_package(tool)
+					if not p:is_installed() then
+						p:install()
+					end
+				end
+			end
+			if mr.refresh then
+				mr.refresh(ensure_installed)
+			else
+				ensure_installed()
+			end
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "saghen/blink.cmp", "williamboman/mason.nvim" },
+
+		-- example calling setup directly for each LSP
+		config = function()
 			vim.diagnostic.config({
 				underline = false,
 				signs = false,
 				update_in_insert = false,
-				virtual_text =  {spacing = 2, prefix = "●"},
+				virtual_text = { spacing = 2, prefix = "●" },
 				severity_sort = true,
 				float = {
-					border = "rounded"
-				}
+					border = "rounded",
+				},
 			})
 
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
-    local lspconfig = require('lspconfig')
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+			local lspconfig = require("lspconfig")
 
-    lspconfig['lua_ls'].setup({ capabilities = capabilities })
-	      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-        callback = function(ev)
-          -- vim.keymap.set("n", "K", vim.lsp.buf.hover) -- configured in "nvim-ufo" plugin
-          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {
-            buffer = ev.buf,
-            desc = "[LSP] Show diagnostic",
-          })
-          vim.keymap.set("n", "<leader>gk", vim.lsp.buf.signature_help, { desc = "[LSP] Signature help" })
-          vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, { desc = "[LSP] Add workspace folder" })
-          vim.keymap.set(
-            "n",
-            "<leader>wr",
-            vim.lsp.buf.remove_workspace_folder,
-            { desc = "[LSP] Remove workspace folder" }
-          )
-          vim.keymap.set("n", "<leader>wl", function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end, { desc = "[LSP] List workspace folders" })
-          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = ev.buf, desc = "[LSP] Rename" })
-        end,
-      })
-  end
-}
+			lspconfig["lua_ls"].setup({ capabilities = capabilities })
+			lspconfig["marksman"].setup({ capabilities = capabilities })
+			lspconfig["ts_ls"].setup({ capabilities = capabilities })
+			-- Use LspAttach autocommand to only map the following keys
+			-- after the language server attaches to the current buffer
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					-- vim.keymap.set("n", "K", vim.lsp.buf.hover) -- configured in "nvim-ufo" plugin
+					vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {
+						buffer = ev.buf,
+						desc = "[LSP] Show diagnostic",
+					})
+					vim.keymap.set("n", "<leader>gk", vim.lsp.buf.signature_help, { desc = "[LSP] Signature help" })
+					vim.keymap.set(
+						"n",
+						"<leader>wa",
+						vim.lsp.buf.add_workspace_folder,
+						{ desc = "[LSP] Add workspace folder" }
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>wr",
+						vim.lsp.buf.remove_workspace_folder,
+						{ desc = "[LSP] Remove workspace folder" }
+					)
+					vim.keymap.set("n", "<leader>wl", function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, { desc = "[LSP] List workspace folders" })
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = ev.buf, desc = "[LSP] Rename" })
+				end,
+			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre",
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				markdown = { "prettierd" },
+				-- Use the "_" filetype to run formatters on filetypes that don't have other formatters configured.
+				["_"] = { "trim_whitespace" },
+			},
+
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		},
+	},
+
+	{
+		"mfussenegger/nvim-lint",
+		event = "BufWritePost",
+		config = function()
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					require("lint").try_lint()
+					require("lint").try_lint("codespell")
+				end,
+			})
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		cmd = "Trouble",
+    -- stylua: ignore
+    keys = {
+      { "<leader>gd", "<CMD>Trouble diagnostics toggle<CR>",                        desc = "[Trouble] Toggle buffer diagnostics"              },
+      { "<leader>gs", "<CMD>Trouble symbols toggle focus=false<CR>",                desc = "[Trouble] Toggle symbols "                        },
+      { "<leader>gl", "<CMD>Trouble lsp toggle focus=false win.position=right<CR>", desc = "[Trouble] Toggle LSP definitions/references/...", },
+      { "<leader>gL", "<CMD>Trouble loclist toggle<CR>",                            desc = "[Trouble] Location List"                          },
+      { "<leader>gq", "<CMD>Trouble qflist toggle<CR>",                             desc = "[Trouble] Quickfix List"                          },
+
+      -- { "grr", "<CMD>Trouble lsp_references focus=true<CR>",         mode = { "n" }, desc = "[Trouble] LSP references"                        },
+      -- { "gD", "<CMD>Trouble lsp_declarations focus=true<CR>",        mode = { "n" }, desc = "[Trouble] LSP declarations"                      },
+      -- { "gd", "<CMD>Trouble lsp_type_definitions focus=true<CR>",    mode = { "n" }, desc = "[Trouble] LSP type definitions"                  },
+      -- { "gri", "<CMD>Trouble lsp_implementations focus=true<CR>",    mode = { "n" }, desc = "[Trouble] LSP implementations"                   },
+    },
+		opts = {},
+
+		config = function(_, opts)
+			require("trouble").setup(opts)
+			local symbols = require("trouble").statusline({
+				mode = "lsp_document_symbols",
+				groups = {},
+				title = false,
+				filter = { range = true },
+				format = "{kind_icon}{symbol.name:Normal}",
+				-- The following line is needed to fix the background color
+				-- Set it to the lualine section you want to use
+				-- hl_group = "lualine_b_normal",
+			})
+
+			-- Insert status into lualine
+			opts = require("lualine").get_config()
+			table.insert(opts.winbar.lualine_b, 1, {
+				symbols.get,
+				cond = symbols.has,
+			})
+			require("lualine").setup(opts)
+		end,
+	},
 }
--- lazy/lua/plugins/ui.lua
+++ lazy/lua/plugins/ui.lua
@@ -1,22 +1,23 @@
 return {
-  {
-    "nvim-tree/nvim-web-devicons",
-    opts = {
-      override = {
-        copilot = {
-          icon = "",
-          color = "#cba6f7", -- Catppuccin.mocha.mauve
-          name = "Copilot",
-        },
-      },
-    },
-  },
-
-	{'nvim-lualine/lualine.nvim',
-	dependencies = {
-		'nvim-tree/nvim-web-devicons'
+	{
+		"nvim-tree/nvim-web-devicons",
+		opts = {
+			override = {
+				copilot = {
+					icon = "",
+					color = "#cba6f7", -- Catppuccin.mocha.mauve
+					name = "Copilot",
+				},
+			},
+		},
 	},
-	opts = {
+
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
 			options = {
 				theme = "catppuccin",
 				always_divide_middle = false,
@@ -24,189 +25,194 @@ return {
 				section_separators = { left = "", right = "" },
 			},
 			sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename" },
-        lualine_x = {},
-        lualine_y = { "encoding", "fileformat", "filetype", "progress" },
-        lualine_z = { "location" },
-      },
-      winbar = {
-        lualine_a = { "filename", },
-        lualine_b = { { function() return " " end, color = "Comment", }, },
-        lualine_x = { "lsp_status", },
-      },
-      inactive_winbar = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = {},
+				lualine_y = { "encoding", "fileformat", "filetype", "progress" },
+				lualine_z = { "location" },
+			},
+			winbar = {
+				lualine_a = { "filename" },
+				lualine_b = { {
+					function()
+						return " "
+					end,
+					color = "Comment",
+				} },
+				lualine_x = { "lsp_status" },
+			},
+			inactive_winbar = {
         -- Always show winbar
         -- stylua: ignore
         lualine_b = { function() return " " end, },
-      },
+			},
 		},
-		    config = function(_, opts)
+		config = function(_, opts)
 			local frappe = require("catppuccin.palettes").get_palette("frappe")
 
-      local function show_macro_recording()
-        local recording_register = vim.fn.reg_recording()
-        if recording_register == "" then
-          return ""
-        else
-          return "󰑋 " .. recording_register
-        end
-      end
+			local function show_macro_recording()
+				local recording_register = vim.fn.reg_recording()
+				if recording_register == "" then
+					return ""
+				else
+					return "󰑋 " .. recording_register
+				end
+			end
 
-      local macro_recording = {
-        show_macro_recording,
-        color = { fg = "#333333", bg = frappe.red },
-        separator = { left = "", right = "" },
-        padding = 0,
-      }
+			local macro_recording = {
+				show_macro_recording,
+				color = { fg = "#333333", bg = frappe.red },
+				separator = { left = "", right = "" },
+				padding = 0,
+			}
 
-      local copilot = {
-        "copilot",
-        show_colors = true,
-        symbols = {
-          status = {
-            hl = {
-              enabled = frappe.green,
-              sleep = frappe.overlay0,
-              disabled = frappe.surface0,
-              warning = frappe.peach,
-              unknown = frappe.red,
-            },
-          },
-          spinner_color = frappe.mauve,
-        },
-      }
+			local copilot = {
+				"copilot",
+				show_colors = true,
+				symbols = {
+					status = {
+						hl = {
+							enabled = frappe.green,
+							sleep = frappe.overlay0,
+							disabled = frappe.surface0,
+							warning = frappe.peach,
+							unknown = frappe.red,
+						},
+					},
+					spinner_color = frappe.mauve,
+				},
+			}
 
-      table.insert(opts.sections.lualine_x, 1, macro_recording)
-      table.insert(opts.sections.lualine_c, copilot)
+			table.insert(opts.sections.lualine_x, 1, macro_recording)
+			table.insert(opts.sections.lualine_c, copilot)
 
-      require("lualine").setup(opts)
-    end,
-  },
+			require("lualine").setup(opts)
+		end,
+	},
 
-  {
-    "romgrk/barbar.nvim",
-    version = "^1.0.0", -- optional: only update when a new 1.x version is released
-    dependencies = {
-      "lewis6991/gitsigns.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    init = function()
-      vim.g.barbar_auto_setup = false
-    end,
-    lazy = false,
+	{
+		"romgrk/barbar.nvim",
+		version = "^1.0.0", -- optional: only update when a new 1.x version is released
+		dependencies = {
+			"lewis6991/gitsigns.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		init = function()
+			vim.g.barbar_auto_setup = false
+		end,
+		lazy = false,
     -- stylua: ignore
     keys = {
       { "<[-b>", "<CMD>BufferPrevious<CR>",     mode = {"n"}, desc = "[Buffer] Previous buffer"   },
       { "<]-b>", "<CMD>BufferNext<CR>",         mode = {"n"}, desc = "[Buffer] Next buffer"       },
     },
-    opts = {
-      animation = true,
-      -- Automatically hide the tabline when there are this many buffers left.
-      -- Set to any value >=0 to enable.
-      auto_hide = 1,
+		opts = {
+			animation = true,
+			-- Automatically hide the tabline when there are this many buffers left.
+			-- Set to any value >=0 to enable.
+			auto_hide = 1,
 
-      -- Set the filetypes which barbar will offset itself for
-      sidebar_filetypes = {
-        NvimTree = true, -- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
-      },
-    },
-  },
+			-- Set the filetypes which barbar will offset itself for
+			sidebar_filetypes = {
+				NvimTree = true, -- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
+			},
+		},
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    keys = {
-      { "<leader>e", "<CMD>NvimTreeToggle<CR>", mode = { "n" }, desc = "[NvimTree] Toggle NvimTree" },
-    },
-    opts = {},
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		keys = {
+			{ "<leader>e", "<CMD>NvimTreeToggle<CR>", mode = { "n" }, desc = "[NvimTree] Toggle NvimTree" },
+		},
+		opts = {},
+	},
 
 	-- 不同括号的颜色区分
 	{
-    "HiPhish/rainbow-delimiters.nvim",
-    main = "rainbow-delimiters.setup",
-    submodules = false,
-    opts = {},
-  },
+		"HiPhish/rainbow-delimiters.nvim",
+		main = "rainbow-delimiters.setup",
+		submodules = false,
+		opts = {},
+	},
 
-	  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      -- {"rcarriga/nvim-notify", opts = {background_colour = "#000000"}}
-    },
-    keys = {
-      { "<leader>sN", "<CMD>Noice pick<CR>", desc = "[Noice] Pick history messages" },
-      { "<leader>N", "<CMD>Noice<CR>", desc = "[Noice] Show history messages" },
-    },
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			-- {"rcarriga/nvim-notify", opts = {background_colour = "#000000"}}
+		},
+		keys = {
+			{ "<leader>sN", "<CMD>Noice pick<CR>", desc = "[Noice] Pick history messages" },
+			{ "<leader>N", "<CMD>Noice<CR>", desc = "[Noice] Show history messages" },
+		},
 
-    opts = {
-      popupmenu = {
-        enabled = false,
-      },
-      notify = {
-        enabled = false,
-      },
-      lsp = {
+		opts = {
+			popupmenu = {
+				enabled = false,
+			},
+			notify = {
+				enabled = false,
+			},
+			lsp = {
 
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
-          ["vim.lsp.util.stylize_markdown"] = false,
-        },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true, -- add a border to hover docs and signature help
-      },
-      routes = {
-        -- Hide search count
-        { filter = { event = "msg_show", kind = "search_count" }, opts = { skip = true } },
-        -- Hide written message
-        { filter = { event = "msg_show", kind = "" }, opts = { skip = true } },
-      },
-    },
-  },
-	  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      ---@type false | "classic" | "modern" | "helix"
-      preset = "helix",
-      win = {
-        -- no_overlap = true,
-        title = false,
-        width = 0.5,
-      },
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+					["vim.lsp.util.stylize_markdown"] = false,
+				},
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = false, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+			routes = {
+				-- Hide search count
+				{ filter = { event = "msg_show", kind = "search_count" }, opts = { skip = true } },
+				-- Hide written message
+				{ filter = { event = "msg_show", kind = "" }, opts = { skip = true } },
+			},
+		},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			---@type false | "classic" | "modern" | "helix"
+			preset = "helix",
+			win = {
+				-- no_overlap = true,
+				title = false,
+				width = 0.5,
+			},
       -- stylua: ignore
       spec = {
         { "<leader>cc", group = "<CodeCompanion>", icon = "" },
         { "<leader>s",  group = "<Snacks>"                    },
         { "<leader>t",  group = "<Snacks> Toggle"             },
       },
-      -- expand all nodes wighout a description
-      expand = function(node)
-        return not node.desc
-      end,
-    },
-    keys = {
+			-- expand all nodes wighout a description
+			expand = function(node)
+				return not node.desc
+			end,
+		},
+		keys = {
       -- stylua: ignore
       { "<leader>?", function() require("which-key").show({ global = false }) end, desc = "[Which-key] Buffer Local Keymaps", },
-    },
-  },
+		},
+	},
 }
